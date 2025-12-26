ac403abca749f56e054aa3bafc0baed39a969afd
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2025-12-26 12:21:09 +0200

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,5 @@
 [BITS 16]
-[ORG 0x7c00]
+[ORG 0x7C00]
 
 start:
     cli
@@ -7,9 +7,9 @@ start:
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00
+    mov sp, 0x7C00
     mov ah, 0x02
-    mov al, 10
+    mov al, 8
     mov ch, 0
     mov cl, 2
     mov dh, 0
@@ -21,4 +21,4 @@ start:
     hlt
 
 times 510 - ($ - $$) db 0
-dw 0xAA55
+db 0x55, 0xAA
