a8c84232657a713b31b9c14d81ff519f12b4ba2c
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2025-12-26 13:02:56 +0200

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,3 +1,5 @@
+%include "libs/macros.asm"
+
 [BITS 16]
 [ORG 0x8000]
 
@@ -5,53 +7,28 @@ start:
     mov ax, 0x00
     mov ds, ax
     mov es, ax
-
-    call clrscr
-
-    call kalloc_init
-
-    mov si, msg1
-    call printk
-    mov si, msg2
-    call printk
-
-    mov bx, 0
-    call kmalloc
+    clrscr
+    allocinit
+    print msg1
+    print msg2
+    alloc 256
     jc alloc_failed
-
-    mov di, ax
-    call scank
-
-    mov si, di
-    call getlen
-    mov [str_len], ax
-
-    mov si, msg3
-    call printk
-    mov si, msg4
-    call printk
-
-    mov si, di
-    call printk
-
-    mov si, msg5
-    call printk
-
-    mov ax, [str_len]
-    call printnumk
-
-    mov bx, di
-    call kfree
-
+    scan ax
+    len di, [str_len]
+    print msg3
+    print msg4
+    print di
+    print msg5
+    printnum [str_len]
+    free di
     cli
     hlt
 
 alloc_failed:
-    mov si, alloc_failed_msg
-    call printk
+    print alloc_failed_msg
 
 %include "libs/printk.asm"
-%include "libs/clrscr.asm" 
+%include "libs/clearscr.asm" 
 %include "libs/scank.asm"
 %include "libs/kalloc.asm"
 %include "libs/getlen.asm"
